b267c0d2ff58853353bf5edce8cdb30be30d13c1
Eric Bower <neurosnap@gmail.com>
2017-05-20 15:06:16 -0400

    we are drawing baby

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
+import 'babel-polyfill';
 import L from 'leaflet';
+import FreeDraw from 'leaflet-freedraw';
 
-const map = L.map('map').setView([42.279594, -83.732124], 13);
+const tileMapUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+const attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+const map = L.map(document.querySelector('.map')).setView([42.279594, -83.732124], 13);
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+L.tileLayer(tileMapUrl, { attribution }, { doubleClickZoom: false })
+ .addTo(map);
+
+const freeDraw = new FreeDraw({ mode: FreeDraw.ALL });
+
+freeDraw.on('markers', (event) => {
+  console.log(event.latLngs);
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') freeDraw.cancel();
+});
+
+map.addLayer(freeDraw);
