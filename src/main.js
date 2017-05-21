44c774d1b080087072001b59bfe9ebc4a1271745
Eric Bower <neurosnap@gmail.com>
2017-05-21 10:29:36 -0400

    remove freedraw, use dra

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import 'babel-polyfill';
 import L from 'leaflet';
-import FreeDraw from 'leaflet-freedraw';
 
 const tileMapUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
 const attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
@@ -8,15 +7,3 @@ const map = L.map(document.querySelector('.map')).setView([42.279594, -83.732124
 
 L.tileLayer(tileMapUrl, { attribution }, { doubleClickZoom: false })
  .addTo(map);
-
-const freeDraw = new FreeDraw({ mode: FreeDraw.ALL });
-
-freeDraw.on('markers', (event) => {
-  console.log(event.latLngs);
-});
-
-document.addEventListener('keydown', (event) => {
-  if (event.key === 'Escape') freeDraw.cancel();
-});
-
-map.addLayer(freeDraw);
