4d522e4b79996ea97c51f4daa259f0f3c1f43a40
Eric Bower <neurosnap@gmail.com>
2018-07-08 22:53:36 -0400

    fix deploy issues

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -x
 
+git remote update
 git reset --hard origin/$BRANCH
 
 docker-compose -f production.yml pull --ignore-pull-failures
-docker-compose -f production.yml build
+docker-compose -f production.yml build --no-cache
 docker-compose -f production.yml up -d
+docker image prune -f
