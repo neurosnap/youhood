8cce422972236694b9a7a4b892bf97cd322fbbe1
Cash Compton <cscmptn@gmail.com>
2018-08-15 17:17:13 -0400

    Close Sign In popup onBlur (#67)
    * Close Sign In popup onBlur
    
    * Fixed closing popup when child clicked

--- packages/ui/signin.test.ts
+++ packages/ui/signin.test.ts
@@ -2,15 +2,17 @@ import { shallow } from 'enzyme';
 import * as h from 'react-hyperscript';
 
 import { SignIn } from './signin';
-import { NavHover } from './ui';
+import { NavHover, SignInEl } from './ui';
 import Profile from './profile';
 import AuthMenu from './auth';
+
+jest.useFakeTimers();
 
 describe('SignIn', () => {
   describe('when user is not authenticated', () => {
     it('should render text `Sign In`', () => {
       const tree = shallow(h(SignIn, { authenticated: false }));
-      expect(tree.find('a').text()).toEqual('Sign In');
+      expect(tree.find(SignInEl).html()).toContain('Sign In');
     });
   });
 
@@ -22,7 +24,7 @@ describe('SignIn', () => {
           user: { email: 'eric@cool.com' },
         }),
       );
-      expect(tree.find('a').text()).toEqual('eric@cool.com');
+      expect(tree.find(SignInEl).html()).toContain('eric@cool.com');
     });
   });
 
@@ -42,7 +44,7 @@ describe('SignIn', () => {
       });
     });
 
-    describe('when the user is authenticated', () => {
+    describe('when the user is not authenticated', () => {
       it('should render SignInMenu component', () => {
         const tree = shallow(
           h(SignIn, {
@@ -54,6 +56,24 @@ describe('SignIn', () => {
         tree.find(NavHover).simulate('click');
 
         expect(tree.find(AuthMenu).length).toEqual(1);
+      });
+    });
+
+    describe('clicking outside of the sign in popup', () => {
+      it('should close the popup', () => {
+        const ev = { currentTarget: { contains: () => false } };
+        const doc = {};
+        const tree = shallow(
+          h(SignIn, {
+            authenticated: false,
+            user: { email: 'eric@cool.com' },
+          }),
+        );
+        tree.setState({ open: true });
+        const instance = tree.instance() as SignIn;
+        instance.onBlur(ev, doc);
+        jest.runAllTimers();
+        expect(tree.state()).toEqual({ open: false });
       });
     });
   });
