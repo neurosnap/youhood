36ea357f87e894b06b2f919598394d97f1508c90
Eric Bower <neurosnap@gmail.com>
2018-09-17 00:10:39 -0400

    try again

--- server/db.js
+++ server/db.js
@@ -14,6 +14,17 @@ log(conn);
 
 const pool = new Pool(conn);
 
+pool.connect((err, client, release) => {
+  if (err) {
+    console.log(err);
+    log(err);
+  } else {
+    console.log('Connected to postgresql!');
+  }
+
+  release();
+});
+
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
