ba61c39c7618f54338d6bccdbabececaaedbad48
Eric Bower <neurosnap@gmail.com>
2018-09-16 23:52:25 -0400

    log more connection info

--- server/db.js
+++ server/db.js
@@ -1,11 +1,18 @@
 const { Pool } = require('pg');
+const debug = require('debug');
 
-const pool = new Pool({
+const log = debug('app:db');
+
+const conn = {
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
   port: process.env.PGPORT,
-});
+  host: process.end.PGHOST,
+};
+log(conn);
+
+const pool = new Pool(conn);
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
