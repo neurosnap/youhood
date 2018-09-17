1f9e1a5a4b00e2fb258cf55acafa304a98864ed1
Eric Bower <neurosnap@gmail.com>
2018-09-16 22:53:24 -0400

    explicitly set pg info in server

--- server/db.js
+++ server/db.js
@@ -1,6 +1,11 @@
 const { Pool } = require('pg');
 
-const pool = new Pool();
+const pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
