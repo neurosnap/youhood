ca6bff41f3c08834a8b31954a373522650df784e
Eric Bower <neurosnap@gmail.com>
2018-10-16 22:59:00 -0400

    only allow users to create hoods when authed

--- packages/ui/draw.ts
+++ packages/ui/draw.ts
@@ -5,15 +5,10 @@ import styled from 'styled-components';
 import { actions, selectors } from '@youhood/hood';
 const { drawHood, cancelDrawHood } = actions;
 const { getIsEditing } = selectors;
+import { selectors as authSelectors } from '@youhood/auth';
+const { isUserAuthenticated } = authSelectors;
 
 import { HoodBarButton } from './ui';
-
-type CancelFn = (event: React.MouseEvent<HTMLElement>) => void;
-interface Props {
-  isEditing: boolean;
-  handleCancelDrawHood: CancelFn;
-  handleDrawHood: (event: React.MouseEvent<HTMLButtonElement>) => void;
-}
 
 const Button = HoodBarButton.extend`
   width: 91px;
@@ -31,27 +26,48 @@ const Overlay = styled.div`
   cursor: pointer;
 `;
 
-export const Cancel = ({ cancelDraw }: { cancelDraw: CancelFn }) =>
+interface ICancel {
+  cancelDraw: CancelFn;
+}
+export const Cancel: React.SFC<ICancel> = ({ cancelDraw }) =>
   h('div', [
     h(Button, { onClick: cancelDraw }, [h('div', 'Cancel')]),
     h(Overlay, { onClick: cancelDraw }),
   ]);
 
-export const DrawHood = ({
+type CancelFn = (event: React.MouseEvent<HTMLElement>) => void;
+interface IState {
+  isUserLoggedIn: boolean;
+  isEditing: boolean;
+}
+interface IDispatch {
+  handleCancelDrawHood: CancelFn;
+  handleDrawHood: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+interface Props extends IState, IDispatch {}
+
+export const DrawHood: React.SFC<Props> = ({
   handleDrawHood,
   handleCancelDrawHood,
   isEditing,
-}: Props) =>
-  isEditing
+  isUserLoggedIn,
+}) => {
+  if (!isUserLoggedIn) {
+    return null;
+  }
+
+  return isEditing
     ? h(Cancel, { cancelDraw: handleCancelDrawHood })
     : h(Button, { onClick: handleDrawHood }, 'Create Hood');
+};
 
-export default connect(
+export default connect<IState, IDispatch>(
   (state) => ({
     isEditing: getIsEditing(state),
+    isUserLoggedIn: isUserAuthenticated(state),
   }),
   (dispatch) => ({
     handleDrawHood: () => dispatch(drawHood()),
     handleCancelDrawHood: () => dispatch(cancelDrawHood()),
   }),
-)(DrawHood as any);
+)(DrawHood);
